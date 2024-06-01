b2f86632d885f4e1756c95c0d342cbb2ec35d63a
santidemo <abansantiago2.0@gmail.com>
2024-06-01 12:06:17 -0300

    Prueba final

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,14 @@
 from django.db import models
 import datetime
 from django_quill.fields import QuillField
+
 class Post(models.Model):
-    title= models.CharField(max_length=100)
-    description= QuillField(default='')
-    image=models.ImageField(upload_to='blog/images')
-    date= models.DateField(datetime.date.today)
-    
+    title = models.CharField(max_length=100)
+    description = QuillField(
+        default='<p></p>',  
+        blank=True,
+        null=True,
+        
+    )
+    image = models.ImageField(upload_to='blog/images')
+    date = models.DateField(default=datetime.date.today)
